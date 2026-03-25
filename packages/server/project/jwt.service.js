beec5921c2137156ec08b670ee484a8821dbf358
indianbill007 <indianbill007@gmail.com>
2026-03-26 01:40:23 +0530

    security: Move hardcoded JWT/AES secrets to env vars, fix .gitignore

--- packages/server/project/jwt.service.js
+++ packages/server/project/jwt.service.js
@@ -1,15 +1,28 @@
 import jwt from 'jsonwebtoken';
 import crypto from "crypto";
 
+const DEV_JWT_SECRET = "eef210e9a7cc26ff1790fb771e2cbeb84e2d0db4d851ace6e2a05d47c664f90a0181429a4f3ab76025637da9a143021b72f1fead6c6c8adfa83cfeae900f26a2";
+const DEV_CRYPTO_PASSWORD = "d%_eQHBl]{E(/TYe>/h#tKe.#_Ah^Q1h";
+
+function getSecret(envVar, devFallback, name) {
+    const value = process.env[envVar];
+    if (value) return value;
+    if (process.env.NODE_ENV === 'production') {
+        throw new Error(`${envVar} must be set in production environment`);
+    }
+    return devFallback;
+}
+
 class JwtAuth {
     constructor() {
-        this.accessTokenSecret = "eef210e9a7cc26ff1790fb771e2cbeb84e2d0db4d851ace6e2a05d47c664f90a0181429a4f3ab76025637da9a143021b72f1fead6c6c8adfa83cfeae900f26a2";
+        this.accessTokenSecret = getSecret('JWT_ACCESS_TOKEN_SECRET', DEV_JWT_SECRET, 'JWT secret');
+        this.cryptoPassword = getSecret('CRYPTO_PASSWORD', DEV_CRYPTO_PASSWORD, 'Crypto password');
         this.tokenLife = '10h';
     }
 
     async verify(token) {
         return new Promise((resolve, reject) => {
-            const decryptedToken = this.decrypt(token, "d%_eQHBl]{E(/TYe>/h#tKe.#_Ah^Q1h");
+            const decryptedToken = this.decrypt(token, this.cryptoPassword);
 
             if (decryptedToken.error) {
                 return reject(decryptedToken.error);
@@ -20,7 +33,7 @@ class JwtAuth {
                 if (err) reject(err);
                 else {
                     if (decoded.signature) {
-                        const decryptedSignature = this.decrypt(decoded.signature, "d%_eQHBl]{E(/TYe>/h#tKe.#_Ah^Q1h");
+                        const decryptedSignature = this.decrypt(decoded.signature, this.cryptoPassword);
                         if (decryptedSignature.error) {
                             return reject(decryptedSignature.error);
                         }
